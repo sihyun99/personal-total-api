56b515c80d72a47a4028f55c776d080897ff9e67
SIAN <fkrtldn1@naver.com>
2023-11-24 14:06:55 +0900

    최초 커밋
    최초커밋. client-server-db 파이프라인 구축을 위한 프로젝트의 BackendServer

---
+++ src/main/java/com/example/demo/TotalApiApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class TotalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication application = new SpringApplication(TotalApiApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
+	}
+
+}
